0c499a77f7bce0634bb8f2b9e379e224dad1fb1b
popolino <polinadorina888@gmail.com>
2025-04-28 19:39:46 +0300

    build

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
 
