40a28654de8b7700f1e1876462be74c1834dbeb8
popolino <polinadorina888@gmail.com>
2025-04-28 16:59:12 +0300

    fix: add express and cors dependencies

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 export default app;
