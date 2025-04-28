d4166ef327452e85610d42cf75b7e4752140b85a
popolino <polinadorina888@gmail.com>
2025-04-28 18:12:56 +0300

    build

--- server/app.js
+++ server/app.js
@@ -14,10 +14,10 @@ app.use('/api', itemsRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 export default app;
