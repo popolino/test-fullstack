1bf776e507ad6dc3321cb1d402e6b8853bf32b43
popolino <polinadorina888@gmail.com>
2025-04-28 19:13:00 +0300

    build

--- server/app.js
+++ server/app.js
@@ -11,13 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', itemsRoutes);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+const buildPath = path.join(process.cwd(), 'client', 'build');
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+app.use(express.static(buildPath));
+app.get('*', (_, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 export default app;
